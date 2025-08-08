3700fe393cc8f43fcc9204c1adba72c37e2b6c7c
Fabio Squizzato <fsquizzato@visualmedica.com>
2025-08-08 11:50:42 -0300

    change: using leaflet instead of bing maps

--- index.js
+++ index.js
@@ -12,20 +12,21 @@ new Vue({
     setInterval(this.obtienePosicion, 1500);
   },
   data: {
-    radianes:0,
+    radianes: 0,
     centigrados: 0,
-    deltaLongi:0,
-    deltaLati:0,
-    pendiente:0,
-    direccion:'',
+    deltaLongi: 0,
+    deltaLati: 0,
+    pendiente: 0,
+    direccion: '',
     cardinal: 0,
-    direccionPrecisa:'',
+    direccionPrecisa: '',
     iss: [],
     spineActive: true,
-    mapaCreado: [],
+    mapaCreado: null,
     centro: [],
-    pushpin: [],
+    pushpin: null,
     posicionAnterior: [],
+    trayectoria: []
   },
   methods: {
     obtienePosicion: function () {
@@ -33,75 +34,80 @@ new Vue({
       axios
         .get("https://bus9ijilni.execute-api.us-east-1.amazonaws.com/latest/iss")
         .then(function (response) {
-          self.posicionAnterior = {lat:self.iss[0].lat,longi:self.iss[0].longi};
-          self.deltaLati=response.data.iss_position.latitude-self.posicionAnterior.lat;
-          self.deltaLongi=response.data.iss_position.longitude-self.posicionAnterior.longi;
-          self.pendiente = self.deltaLati/self.deltaLongi;
+          self.posicionAnterior = { lat: self.iss[0].lat, longi: self.iss[0].longi };
+          self.deltaLati = response.data.iss_position.latitude - self.posicionAnterior.lat;
+          self.deltaLongi = response.data.iss_position.longitude - self.posicionAnterior.longi;
+          self.pendiente = self.deltaLati / self.deltaLongi;
           self.radianes = Math.atan(self.pendiente);
-          self.centigrados = self.radianes*180/(Math.PI);
+          self.centigrados = self.radianes * 180 / (Math.PI);
           self.cardinal = self.centigrados < 90 ? 90 - self.centigrados : 450 - self.centigrados;
-          if(self.deltaLongi > 0){
-            if(self.pendiente < 1 && self.pendiente > -1){
+
+          if (self.deltaLongi > 0) {
+            if (self.pendiente < 1 && self.pendiente > -1) {
               self.direccion = 'ESTE'
-            }else if(self.pendiente > 1){
+            } else if (self.pendiente > 1) {
               self.direccion = 'NORTE'
-            }else if(self.pendiente < -1){
+            } else if (self.pendiente < -1) {
               self.direccion = 'SUR'
             }
-          }else{
+          } else {
             self.direccion = 'AVANZANDO HACIA EL OESTE'
           }
-          //DIRECCIÓN PRECISA
-          if(self.deltaLongi > 0){
 
-          }else{
-            self.direccionPrecisa = 'AVANZANDO HACIA EL OESTE'
-          }
           self.iss.pop();
           self.iss.push({
             lat: response.data.iss_position.latitude,
             longi: response.data.iss_position.longitude,
           });
-          self.mapaCreado.setView({
-            center: new Microsoft.Maps.Location(
-              response.data.iss_position.latitude,
-              response.data.iss_position.longitude
-            ),
-          });
-          self.center = self.mapaCreado.getCenter();
-          self.pushpin.setLocation(self.center);
-          var coor = [
-            self.center,
-            new Microsoft.Maps.Location(
-              self.posicionAnterior.lat,
-              self.posicionAnterior.longi
-            ),
-            
-          ];
-          var line = new Microsoft.Maps.Polyline(coor, {
-            strokeColor: "red",
-            strokeThickness: 5,
-            strokeDashArray: [4, 4]
-          });
-          self.mapaCreado.entities.push(line);
+
+          // Actualizar la posición del mapa y el marcador
+          self.mapaCreado.setView([response.data.iss_position.latitude, response.data.iss_position.longitude]);
+          self.pushpin.setLatLng([response.data.iss_position.latitude, response.data.iss_position.longitude]);
+
+          // Dibujar la línea de trayectoria
+          var nuevaLinea = L.polyline([
+            [self.posicionAnterior.lat, self.posicionAnterior.longi],
+            [response.data.iss_position.latitude, response.data.iss_position.longitude]
+          ], {
+            color: 'red',
+            weight: 5,
+            dashArray: '4, 4'
+          }).addTo(self.mapaCreado);
+
+          // Opcional: mantener un registro de las líneas para poder eliminarlas después si es necesario
+          self.trayectoria.push(nuevaLinea);
+
+          // Opcional: limitar el número de líneas mostradas para mejorar el rendimiento
+          if (self.trayectoria.length > 50) {
+            self.mapaCreado.removeLayer(self.trayectoria.shift());
+          }
         });
     },
     creaMapa: async function () {
       var self = this;
-      this.mapaCreado = new Microsoft.Maps.Map("#myMap", {
-        credentials:
-          "Auy5v8ublpzBYNHJHWATcR9iRYo8xlBc-t3lAb-hTQ2u2pycKtsiU_9ZrX6_TY-a",
-        center: new Microsoft.Maps.Location(-32.906, -68.844),
-        zoom: 11,
+      
+      // Inicializar el mapa de Leaflet
+      this.mapaCreado = L.map('myMap').setView([-32.906, -68.844], 11);
+      
+      // Añadir la capa de OpenStreetMap
+      L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        attribution: '© OpenStreetMap contributors'
+      }).addTo(this.mapaCreado);
+
+      // Crear un icono personalizado para el satélite
+      var sateliteIcon = L.icon({
+        iconUrl: 'satelite.svg',
+        iconSize: [30, 30],
+        iconAnchor: [15, 15]
       });
-      this.mapaCreado.setView({
-        center: new Microsoft.Maps.Location(self.iss[0].lat, self.iss[0].longi),
-      });
-      this.centro = this.mapaCreado.getCenter();
-      this.pushpin = new Microsoft.Maps.Pushpin(self.centro, {
-        icon: "satelite.svg",
-      });
-      this.mapaCreado.entities.push(self.pushpin);
-    },
-  },
+
+      // Crear y añadir el marcador
+      this.pushpin = L.marker([self.iss[0].lat, self.iss[0].longi], {
+        icon: sateliteIcon
+      }).addTo(this.mapaCreado);
+
+      // Inicializar el array para la trayectoria
+      this.trayectoria = [];
+    }
+  }
 });
